df50d3b98c9c2032a7e3c5d47668818e8533b713
anotherD00D <juan.solis@dusty.tamiu.edu>
2025-10-04 00:04:08 -0500

    proj-studentDB complete, code restructuring might be recommended by creating a class data cluster instead of seperate dataframes in main

--- Training/py-Database/hwl-solis.py
+++ Training/py-Database/hwl-solis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from contextlib import redirect_stdout
 
 def processFile(file_name:str):
     raw_read = pd.read_csv(file_name, header=None, names=['raw'])
@@ -24,7 +25,7 @@ def createStudentDataframes(raw_df:pd.DataFrame):
     courses_df['Number'] = courses_df['Number'].astype(int)
 
     GPA_df = getGPADataFrame(student_df, courses_df)
-    
+
     return student_df, courses_df, GPA_df
 
 def getGPADataFrame(student_df:pd.DataFrame, courses_df:pd.DataFrame):
@@ -59,6 +60,7 @@ def createStudentDict(student_df:pd.DataFrame, GPA_df:pd.DataFrame):
     student_dict = df.set_index('Student ID').to_dict(orient="index")
     student_dict = {outer_key: tuple(inner_dict.values())
                     for outer_key, inner_dict in student_dict.items()}
+    
     return student_dict
 
 def createRoster(student_df:pd.DataFrame, courses_df:pd.DataFrame, department:str, course_number:int):
@@ -87,21 +89,83 @@ def printStudents(student_df:pd.DataFrame, GPA_df:pd.DataFrame):
     df = student_df.merge(GPA_df, on='Student ID')
     df = df[['Student ID', 'First Name', 'Last Name', 'Major', 'GPA']].sort_values(by='Student ID').reset_index(drop=True)
     print(df)
+    save([df], file_prefix, output_file)
 
-def addStudent():
+def addStudent(file_name:str, id_number:int, first_name:str, last_name:str, major:str, number:int, courses:list):
+    number = 2
+    write_line = f'{id_number} {first_name} {last_name} {major} {number}'
+    for single_course in courses:
+        write_line += f' {single_course[0]} {single_course[1]} {single_course[2]} {single_course[3]}'
+    
+    with open(f"{file_name}", "a") as f:
+        f.write(f"\n{write_line}")
     return -1
 
-
-
+def save(value_list:list, file_prefix, file_name):
+    out_path = rf"{file_prefix}\{file_name}"
+    fill_in = '='
+    for value in value_list:
+        with open(out_path, 'a') as out, redirect_stdout(out):
+            print(value, end=f"\n\n{fill_in*100}\n\n")
 
 if __name__ == '__main__':
+    isTest = True
+    file_prefix = r'Training\py-Database'
+
+    #Step 1:
+    if isTest:
+        user_name = 'Juan Solis'
+        input_file, output_file = r"studentsDB.txt", r"output_file.txt"
+    else:
+        user_name = input("Please Enter User Name")
+        input_file, output_file = input("Provide the file name of the input file:"), input("Provide the file name of the output file:")
+
+    #Step 2:
+    addStudent(rf'{file_prefix}\{input_file}', 7444, 'Khaled', 'Enab', 'PE', 2, [('CSCE', 1336, 5, 'D-'),('PE', 3300, 3, 'A')])
+    addStudent(rf'{file_prefix}\{input_file}', 5564, 'Elvira', 'Teran', 'CSCE', 1, [('CSCE', 1336, 5, 'A')])
+
+    #Step 3:
+    save([f"User Name:{user_name}",f"Input File:{input_file}"], file_prefix, output_file)
+
+    #Step 4:
+    student_df, courses_df, GPA_df = processFile(rf'{file_prefix}\{input_file}')
+
+    #Step 5:
+    save([student_df, courses_df, GPA_df], file_prefix, output_file)
     
-    file_name = r"Training\py-Database\studentsDB.txt"
-    student_df, courses_df, GPA_df = processFile(file_name)
+    #Step 6:
     student_dict = createStudentDict(student_df, GPA_df)
-    roster_set = createRoster(student_df, courses_df, "CSCE", 3390)
+    
+    #Step 7:
+    save([student_dict], file_prefix, output_file)
+    
+    #Step 8:
+    roster_set = createRoster(student_df, courses_df, "CSCE", 1336)
+    
+    #Step 9:
+    save([roster_set], file_prefix, output_file)
+    
+    #Step 10:
+    roster_set = createRoster(student_df, courses_df, "Math", 2340)
+    
+    #Step 11:
+    save([roster_set], file_prefix, output_file)
+
+    #Step 12:
     course_set = createCourseSet(courses_df, "CSCE")
+    
+    #Step 13:
+    save([course_set], file_prefix, output_file)
+
+    #Step 14:
+    course_set = createCourseSet(courses_df, "Math")
+    
+    #Step 15:
+    save([course_set], file_prefix, output_file)
+
+    #Step 16:
     printStudents(student_df, GPA_df)
+
 
         
     
